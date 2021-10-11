9850de459ecbb1f690ee8e4bcf4362c5ce35effa
nithyamani <nithya.mn999@gmail.com>
2021-10-11 18:51:29 +0530

    removed creds and app id

--- spechelp.rb
+++ spechelp.rb
@@ -5,7 +5,7 @@ def setup_driver
       caps['device'] = ENV['device']
       caps['os_version'] = ENV['os_version']
       caps['realMobile'] = true
-      caps['app'] = 'app_url'
+      caps['app'] = 'BROWSERSTACK_APP_ID'
 
       caps['project'] = ENV['project'] 
       caps['build'] = ENV['build'] 
